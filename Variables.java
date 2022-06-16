a104d89749beb243b2083cb04cc61668f1b07f35
AlexRios <riosalexandra91@gmail.com>
2022-06-15 23:31:58 -0500

    updated prompt message

--- Variables.java
+++ Variables.java
@@ -9,7 +9,7 @@ public class Variables {
 
         var subtraction = 7;
         int answer;
-        String prompt =". Press ENTER when ready.";
+        String prompt =". Don't type in the answer, just press ENTER when you're ready to continue.";
         System.out.println("Think of a number between 1 and 10" + prompt);
         scanner.nextLine();
         System.out.println("Multiply your number by " + firstNumber + prompt);
