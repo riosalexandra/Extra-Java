94c962b25cdd24ad164377b442aca25370b334cf
AlexRios <riosalexandra91@gmail.com>
2022-06-17 23:07:26 -0500

    update

--- Variables.java
+++ Variables.java
@@ -1,14 +1,19 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Variables {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int firstNumber = 2;
-        int secondNumber = 5;
+        Random randomGenerator = new Random();
 
-        var subtraction = 7;
-        int answer;
+
+        int firstNumber = randomGenerator.nextInt(8) +2;
+        int secondNumber = randomGenerator.nextInt(8) +2;
+
+        var subtraction = randomGenerator.nextInt(8) +2;
+        int answer = firstNumber * secondNumber - subtraction;
+
         String prompt =". Don't type in the answer, just press ENTER when you're ready to continue.";
         System.out.println("Think of a number between 1 and 10" + prompt);
         scanner.nextLine();
@@ -20,8 +25,9 @@ public class Variables {
         scanner.nextLine();
         System.out.println("Now subtract 7" + prompt);
         scanner.nextLine();
+        System.out.println("That's the end of our game now...");
+        scanner.nextLine();
 
-        answer = firstNumber * secondNumber - subtraction;
         System.out.println("Tha answer is " + answer);
 
         scanner.close();
